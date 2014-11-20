3514af0ae4b28ae76b6095769d46adcc2e75e29c
Grant Mathews <gmathews@cpan.org>
2014-11-19 19:16:47 -0800

    Fix COUNT(DISTINCT col) without GROUP BY
    Assign a value for the key used to enforce uniqueness for DISTINCT
    columns in do_calc(). This ensures the logic to skip seen column values
    works correctly, especially in the absence of GROUP BY.

--- lib/SQL/Statement.pm
+++ lib/SQL/Statement.pm
@@ -2054,9 +2054,10 @@ sub do_calc()
 
             if ( $coldef->{type} eq 'setfunc' )
             {
-                next
-                  if ( ( $coldef->{distinct} eq 'DISTINCT' )
-                       && defined( $result->{uniq}->[$colidx]->{$colval} ) );
+                if ( $coldef->{distinct} eq 'DISTINCT' ) {
+                    next if defined( $result->{uniq}->[$colidx]->{$colval} );
+                    $result->{uniq}->[$colidx]->{$colval} = 1;
+		}
 
                 $result->{agg}->[$colidx] = clone($empty_agg)
                   unless ( defined( _HASH( $result->{agg}->[$colidx] ) ) );
